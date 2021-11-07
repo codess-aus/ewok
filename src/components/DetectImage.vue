c9cb655ed981655c111368b15e7aeccf9215f3b2
Michelle Mei-Ling Sandford <msandford@outlook.com>
2021-11-07 15:22:09 +0800

    no real change

--- src/components/DetectImage.vue
+++ src/components/DetectImage.vue
@@ -11,7 +11,7 @@
       v-for="pred in predictions"
       :key="pred.index"
     >{{ pred.label }}: {{ pred.probability.toFixed(0) + '%' }}</div>
-    <div v-if="!predictions.length">Thinking.....</div>
+    <div v-if="!predictions.length">Assessing.....</div>
   </div>
 </template>
 
